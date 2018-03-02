c71ee35f9032eeb8a43d89da78771f56b3d5789e
Antti Leppä <antti.leppa@metatavu.fi>
2018-03-02 19:08:46 +0200

    Added support for saving metaform revisions

--- metaform/metaform-ajax.php
+++ metaform/metaform-ajax.php
@@ -2,13 +2,19 @@
   defined( 'ABSPATH' ) || die( 'No script kiddies please!' );
 
   add_action('wp_ajax_save_metaform', function () {
-    error_log("DuUUR");
-
     $id = $_POST['id'];
     $values = $_POST['values'];
     $userId = wp_get_current_user()->ID;
     update_user_meta($userId, "metaform-$id-values", $values);
     wp_die();
   });
-  
+
+  add_action('wp_ajax_save_metaform_revision', function () {
+    $id = $_POST['id'];
+    $userId = wp_get_current_user()->ID;
+    $values = get_user_meta($userId, "metaform-$id-values", true);
+    update_user_meta($userId, "metaform-$id-revision-values", $values);
+    wp_die();
+  });
+
 ?>
