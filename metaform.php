38834d2b2db27e86d09ed3c3e8b00f2d0712175f
Antti Leppä <antti.leppa@metatavu.fi>
2018-06-06 11:59:41 +0300

    Update metaform.php

--- metaform.php
+++ metaform.php
@@ -3,7 +3,7 @@
  * Created on Feb 28, 2018
  * Plugin Name: Wordpress Metaform integration
  * Description: Wordpress integration for Metaforms
- * Version: 0.0.10
+ * Version: 0.0.11
  * Author: Metatavu Oy
  */
 
