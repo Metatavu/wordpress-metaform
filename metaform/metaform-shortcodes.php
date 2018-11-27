04dfb46394be61e1e4ff90b937feb468270efcef
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2018-11-27 07:59:41 +0200

    Fixed loading scripts

--- metaform/metaform-shortcodes.php
+++ metaform/metaform-shortcodes.php
@@ -21,6 +21,17 @@
        * Constructor
        */
       public function __construct() {
+        add_shortcode('metaform', [$this, 'metaformShortcode']);
+        wp_enqueue_style('font_awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' );
+        wp_enqueue_style('jquery-ui', '//cdn.metatavu.io/libs/jquery-ui/1.12.1/jquery-ui.min.css');
+        wp_enqueue_style('flatpickr', '//cdn.metatavu.io/libs/flatpickr/4.0.6/flatpickr.min.css');
+        wp_enqueue_style('hyperform', '//cdn.metatavu.io/libs/hyperform/0.8.15/hyperform.min.css');
+        wp_enqueue_style('animate-css', 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css');
+        wp_register_style('metaform', "//cdn.metatavu.io/libs/metaform-fields/0.6.24/css/form.min.css");
+
+        if (!is_admin()) {
+          wp_enqueue_style('bootstrap-css', '//cdn.metatavu.io/libs/bootstrap/4.1.0/css/bootstrap.min.css');
+        }
       }
       
       /**
@@ -28,13 +39,7 @@
        */
       public function loadScripts() {
         $metaformUrl = '//cdn.metatavu.io/libs/metaform-fields/0.6.24';
-        
-        wp_enqueue_style('font_awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' );
-        wp_enqueue_style('jquery-ui', '//cdn.metatavu.io/libs/jquery-ui/1.12.1/jquery-ui.min.css');
-        wp_enqueue_style('flatpickr', '//cdn.metatavu.io/libs/flatpickr/4.0.6/flatpickr.min.css');
-        wp_enqueue_style('hyperform', '//cdn.metatavu.io/libs/hyperform/0.8.15/hyperform.min.css');
-        wp_enqueue_style('animate-css', 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css');
-        wp_register_style('metaform', "$metaformUrl/css/form.min.css");
+
         wp_enqueue_style('metaform');
 
         wp_register_script('moment', "//cdn.metatavu.io/libs/moment/2.17.1/moment-with-locales.js");
@@ -51,18 +56,13 @@
         wp_register_script('metaform-modernizr', "$metaformUrl/js/modernizr.js");
         wp_register_script('metaform-client', "$metaformUrl/js/metaform-client.min.js", ['jquery', 'jquery-ui-dialog', 'jquery-ui-tabs', 'flatpickr', 'flatpickr-fi', 'metaform-form', 'metaform-utils', 'metaform-modernizr', 'jquery-ui_touch-punch']);
 
-        add_shortcode('metaform', [$this, 'metaformShortcode']);
-
         wp_register_script('metaform-init', plugin_dir_url(dirname(__FILE__)) . 'metaform-init.js', ['metaform-client']);
         wp_localize_script('metaform-init', 'metaformwp', [ 'ajaxurl' => admin_url( 'admin-ajax.php' ) ]);
 
+        wp_enqueue_script('metaform-init');
         wp_register_script('metaform-js', plugin_dir_url(dirname(__FILE__)) . '/metaform/js/metaform.js', ['jquery']);
         wp_localize_script('metaform-js', 'metaformwp', [ 'ajaxurl' => admin_url( 'admin-ajax.php' ) ]);
         wp_enqueue_script('metaform-js');
-
-        if (!is_admin()) {
-          wp_enqueue_style('bootstrap-css', '//cdn.metatavu.io/libs/bootstrap/4.1.0/css/bootstrap.min.css');
-        }
       }
 
       /**
