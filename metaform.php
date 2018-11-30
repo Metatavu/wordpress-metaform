5e9d35ac8042bb1457c3ac005ab9ed9f74bf036f
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2018-11-30 10:32:27 +0200

    Update metaform.php

--- metaform.php
+++ metaform.php
@@ -3,7 +3,7 @@
  * Created on Feb 28, 2018
  * Plugin Name: Wordpress Metaform integration
  * Description: Wordpress integration for Metaforms
- * Version: 0.1.4
+ * Version: 0.1.5
  * Author: Metatavu Oy
  */
 
